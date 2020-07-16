0aa937523a4b62a56071858489d74442656910de
Dong Xia <dong7777xia@gmail.com>
2020-07-16 19:24:39 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,22 @@
 def new_hash
   # return an empty hash
+  food = {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  food = {:pizza => 4}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  people = {:name => "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  {:id => number}
+
+
 end
