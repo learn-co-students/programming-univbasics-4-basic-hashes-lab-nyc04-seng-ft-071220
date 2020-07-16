a4a6626caffdf6823090d08d39331adc23f39716
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-07-16 17:47:18 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,27 @@
 def new_hash
+  new_hash= Hash.new
   # return an empty hash
 end
 
 def my_hash
+  my_hash={
+    name: "Kalkidan",
+    age: 100
+  }
   # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
+  pioneer= {
+    :name => "Grace Hopper",
+  }
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
+  new_hash={
+    :id => number
+  }
   # return a hash with a key :id assigned to the provided number
 end
